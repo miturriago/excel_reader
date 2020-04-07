18b7bee00079797854ff1e49f2df6a883035d6a3
Mauricio Maldonado Iturriago <mauriciomaldo@hotmail.es>
2020-04-06 21:42:16 -0500

    excel updated

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import XLSX from 'xlsx'
-Vue.use(XLSX)
+import JsonExcel from 'vue-json-excel'
+ 
+Vue.component('downloadExcel', JsonExcel)
+
+
 
 Vue.config.productionTip = false
 
